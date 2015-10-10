2358718d72ad148c038a5100953ded0c762cdb98
Komei Shimamura <komei.t.f@gmail.com>
2015-10-10 23:36:18 +0900

    Add the token and the regular expressoin save functionality

--- github-bugspots.js
+++ github-bugspots.js
@@ -3,22 +3,39 @@
 
   var mainTemplateUrl = chrome.extension.getURL('templates/main.html');
   var buttonTemplateUrl = chrome.extension.getURL('templates/button.html');
-  var re =/^http[s]?:[/]+github.com[/]*([^/]*)\/([^/]*)[/|#].*$/;
+  var re = /^http[s]?:[/]+github.com[/]*([^/]*)\/([^/]*)[/|#].*$/;
   var parsedUrl = re.exec(location.href);
   var userName = parsedUrl[1];
   var repoName = parsedUrl[2];
-  var githubClient = new Github({'apiUrl': 'https://api.github.com'});
+
+  /* currentTimestamp is used for calculation of score */
+  var currentTimestamp = (new Date()).getTime();
+
+  /* bugDetectionRegex is used for judging whether given commits are bug fix or not */
+  var bugDetectionRegexString = localStorage.getItem('bugspots-bug-detection-regex') || '[f|F]ix(es|ed)?|[C|c]lose(s|d)?]';
+  var bugDetectionRegex = new RegExp(bugDetectionRegexString);
+
+  /* accessToken is used for accessing private repositories */
+  var accessToken = localStorage.getItem('bugspots-access-token');
+
+  var githubClient = new Github({
+    apiUrl: 'https://api.github.com',
+    token: accessToken,
+    auth: "oauth"
+  });
+
   var repo = githubClient.getRepo(userName, repoName);
 
   function renderButton() {
-    $.get(buttonTemplateUrl, null, function (loadedHtml) {
+    return $.get(buttonTemplateUrl, function (loadedHtml) {
       var templateHtmlString = $(loadedHtml).html();
       $('.tooltipped.tooltipped-w[aria-label=Graphs]').parent().append(templateHtmlString);
 
       var bugspotsButton = $('.js-selected-navigation-item[aria-label=Bugspots]');
 
       bugspotsButton.click(selectNavigationItem);
-      bugspotsButton.click(renderMain);
+      //bugspotsButton.click(renderMain);
+      bugspotsButton.click(getCommits);
 
       function selectNavigationItem() {
         $('.js-selected-navigation-item.selected').removeClass('selected');
@@ -27,36 +44,104 @@
     });
   }
 
-  function renderMain() {
-    $.get(mainTemplateUrl, null, function (loadedHtml) {
+  function renderMain(renderInfo) {
+    renderInfo['bugDetectionRegex'] = bugDetectionRegexString;
+    renderInfo['accessToken'] = accessToken;
+    console.log(renderInfo);
+
+    return $.get(mainTemplateUrl, function (loadedHtml) {
       var templateHtmlString = $(loadedHtml).html();
-      var template = $.templates(templateHtmlString);
-      var resultHtmlString = template.render({"name": "test"});
+      var template = Handlebars.compile(templateHtmlString);
+      var resultHtmlString = template(renderInfo);
       $('#js-repo-pjax-container').html(resultHtmlString);
-      getSourceTree();
-      getCommits();
+      activateJsEventHandler();
     });
   }
 
-  function getSourceTree() {
-    repo.getTree('master?recursive=true', renderSourceTable);
+  function activateJsEventHandler(){
+    $('#jsBugspotsSaveButton').click(function(){
+      /* update bug detection regex */
+      bugDetectionRegexString = $('#bugspotsRegexField').val();
+      localStorage.setItem('bugspots-bug-detection-regex', bugDetectionRegexString);
+      bugDetectionRegex = new RegExp(bugDetectionRegexString);
+
+      /* update token */
+      accessToken = $('#bugspotsTokenField').val();
+      localStorage.setItem('bugspots-access-token', accessToken);
+    });
   }
 
   function renderSourceTable(err, tree) {
-    console.log(tree);
-    var files = _.filter(tree, function(i){ return i.type == "blob" });
+    var files = _.filter(tree, function (i) {
+      return i.type == "blob"
+    });
     console.log(files);
   }
 
   function getCommits() {
-    repo.getCommits({
-      'sha': 'master',
-      'perpage': 100
-    }, analyzeCommits);
+    //var _test = {
+    //  summary: [
+    //    { path: "Gemfile", score: 0.00000614417460221472, url: "https://api.github.com/repos/travelist/dependency-inspector/git/blobs/8975a62227846f6aed69702e4ed982323bde707d" },
+    //    { path: "Gemfile", score: 0.00000614417460221472, url: "https://api.github.com/repos/travelist/dependency-inspector/git/blobs/8975a62227846f6aed69702e4ed982323bde707d" }
+    //  ]
+    //};
+    //renderMain(_test);
+    repo.getCommits({'sha': 'master', 'perpage': 100}, analyzeCommits);
   }
 
-  function analyzeCommits(err, commits){
-    console.log(commits);
+  function analyzeCommits(err, commits) {
+    var info = [];
+    console.log(commits)
+    var fix_commits = _.filter(commits, function (c) {
+      return Boolean(c.commit.message.match(bugDetectionRegex));
+    });
+    console.log(fix_commits);
+    var oldestCommitTimestamp = (new Date(fix_commits[fix_commits.length - 1].commit.author.date)).getTime();
+
+    $.each(fix_commits, function (i, v) {
+      $.get(v.commit.tree.url + '?recursive=true').then(function (r) {
+
+        var timestamp = (new Date(v.commit.author.date)).getTime();
+        var normalizedTimestamp = (timestamp - oldestCommitTimestamp) / currentTimestamp;
+        var score = 1 / (1 + Math.exp(12.0 * (1.0 - normalizedTimestamp)));
+        var files = _.filter(r.tree, function (t) {
+          return t.type == "blob"
+        });
+
+        $.each(files, function (i, v) {
+          info.push({path: v.path, url: v.url, timestamp: timestamp, score: score});
+        });
+
+      }).then(function (r) {
+        if (i == fix_commits.length - 1) {
+          var summary = {};
+
+          $.each(info, function (i, v) {
+            console.log(v);
+            if (v.path in summary) {
+              summary[v.path].score += v.score;
+            }
+            else {
+              summary[v.path] = v;
+            }
+          });
+
+          var summaryInfo = [];
+          $.each(summary, function (k, v) {
+            summaryInfo.push({path: k, score: v.score, url: v.url})
+          });
+          renderMain({summary: summaryInfo});
+        }
+      });
+    });
+  }
+
+  function _saveObject(key, obj) {
+    return localStorage.setItem(key, JSON.stringify(obj));
+  }
+
+  function _getObject(key) {
+    return JSON.parse(localStorage.getItem(key));
   }
 
   function initialize() {
@@ -64,5 +149,4 @@
   }
 
   initialize();
-
 })();
